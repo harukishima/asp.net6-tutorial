b468f2aa0474668e40a9f1ef3f151ecfbe16dd9d
HaruKishima <18120256@student.hcmus.edu.vn>
2022-03-31 23:17:09 +0700

    Initial commit

---
+++ rider-starter-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rider_starter_app.Models;
+
+namespace rider_starter_app.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+}
